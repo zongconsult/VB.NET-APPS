ace2b38b89e2a1e2c1a7cedae81983f7b70f1668
zongconsult <zongconsultinc@gmail.com>
2023-03-14 00:07:21 +0000

    Added Dependencies
    I have added dependencies for the software
    2. I have also added the setups for the softwares
    3. I removed returns books source code and leaving the bin folder which contains the app

--- ReturnsBuk/ReturnsBuk/frmReports.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class frmReports
-
-    Private Sub frmReports_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Me.CenterToScreen()
-
-    End Sub
-End Class
